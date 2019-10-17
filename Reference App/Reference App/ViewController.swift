efbdf04d32a0d22481854c4139d1f47ed8ded51d
pkochar1307 <44370199+pkochar1307@users.noreply.github.com>
2019-10-17 12:30:51 +0530

    Add files via upload

---
+++ Reference App/Reference App/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Reference App
+//
+//  Created by MacMini03 on 17/10/19.
+//  Copyright © 2019 MacMini03. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    //Reference to the button
+    @IBOutlet weak var getData1: UIButton!
+    // To write code for action after clicking on this button
+    
+    @IBAction func getData(_ sender: Any) {
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
